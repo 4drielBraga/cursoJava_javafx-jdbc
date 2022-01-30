666de79371bbe44f5659411f0153177aba440349
Adriel Braga <marcos.adrielbraga@gmail.com>
2022-01-30 17:30:17 -0400

    Deploy DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,10 +1,13 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
 import javafx.beans.property.Property;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -13,8 +16,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
+	
+	private DepartmentService serviceDepartment;
 	
 	@FXML
 	private TableView<Department> tableViewDepartment;
@@ -28,6 +34,12 @@ public class DepartmentListController implements Initializable {
 	
 	@FXML
 	private Button btNew;
+	
+	private ObservableList<Department> obsList;
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.serviceDepartment = service;
+	}
 	
 	@FXML
 	public void onBtNewAction() {
@@ -43,12 +55,19 @@ public class DepartmentListController implements Initializable {
 	
 	private void initializeNodes() {
 		tableColumId.setCellValueFactory(new PropertyValueFactory<>("id"));
-		tableColumId.setCellValueFactory(new PropertyValueFactory<>("Name"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("Name"));
 		
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 	}
 	
-	
+	public void updateTableView() {
+		if(serviceDepartment == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list =	serviceDepartment.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
+	}
 
 }
